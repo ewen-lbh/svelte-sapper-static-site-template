5d8e1d4d337e6419eee89b44c886444db2147b8f
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-06-28 16:47:34 +0200

    🎉 Initial commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let segment;
+</script>
+
+<style>
+</style>
+
+<svelte:head>
+	<title>{segment || ""} TODO:NAME</title>
+</svelte:head>
+
+<main>
+	<slot></slot>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<div class="centered">
+	<h1>The minimal "Svelte For Static Websites" template.</h1>
+	<p>Things to remove before starting:</p>
+	<ul>
+		<li>Code inside <code>src/routes/index.svelte</code></li>
+		<li>Nothing else.</li>
+	</ul>
+</div>
+
+<style>
+.centered {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	width: 100vw;
+	height: 100vh;
+}
+
+h1 {
+	text-align: center;
+}
+</style>
